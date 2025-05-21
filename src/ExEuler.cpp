d9dd91de0cb7ecfd6269f555a58fe05cb8739a4b
Joseph Whittier <josephwhittier.23@gmail.com>
2025-05-21 13:07:06 -0600

    modified:   src/ExEuler.cpp

--- src/ExEuler.cpp
+++ src/ExEuler.cpp
@@ -4,10 +4,27 @@
 
 using namespace std;
 
+void readFile(string fileName){
+    // creating input stream
+    ifstream fin(fileName);
+    if (fin.fail()){
+        cout << "FAILED TO READ FILE" << endl;
+        return;
+    }
+
+    // reading data
+    float dt;
+    float MAX;
+    fin >> dt >> MAX;
+
+    // closing file
+    fin.close();
+}
+
 int main(){
+    readFile("params.dat");
 
 
 
-    
     return 0;
 }
