7d6a9a6174f0f5c24ea4b15d5f70467b27e9e73e
Joseph Whittier <josephwhittier.23@gmail.com>
2025-05-21 15:29:26 -0600

    new file:   .vscode/tasks.json 	new file:   data/output.dat 	new file:   data/params.dat 	new file:   src/ExEuler 	modified:   src/ExEuler.cpp
    	new file:   .vscode/tasks.json
    	new file:   data/params.dat
    	new file:   src/ExEuler
    	modified:   src/ExEuler.cpp

--- src/ExEuler.cpp
+++ src/ExEuler.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <climits>
 #include <math.h>
 
 // setting maximum number of timesteps (2^27)
@@ -7,8 +8,37 @@
 
 using namespace std;
 
+// creating global variables
 float dt;
 int steps;
+
+void readFile(string fileName);
+
+void run(double* &t, double* &x);
+
+void saveFile(double t[MAX], double x[MAX], string fileName);
+
+int main(){
+    string inputFile = "../data/params.dat";
+    readFile(inputFile);
+
+    // calculating number of steps in 9 seconds
+    steps=int(9/dt);
+
+    // creating arrays for time and position
+    double* t;
+    double* x;
+
+    t = new double[MAX];
+    x = new double[MAX];
+
+    run(t,x);
+
+    string outputFile = "../data/output.dat";
+    saveFile(t,x,outputFile);
+
+    return 0;
+}
 
 void readFile(string fileName){
     // creating input stream
@@ -23,23 +53,25 @@ void readFile(string fileName){
 
     // closing file
     fin.close();
+
     return;
 }
 
-void run(float (&t)[MAX], float (&x)[MAX]){
+void run(double* &t, double* &x){
     // initial state of time and position
-    t[0]=0;
-    x[0]=1;
+    t[0]=0.0;
+    x[0]=1.0;
 
     // running Euler integration
     for(int i=1;i<steps;i++){
         t[i]=t[i-1]+dt;
         x[i]=(1-3*dt)*x[i-1];
     }
+
     return;
 }
 
-void saveFile(float t[MAX], float x[MAX], string fileName){
+void saveFile(double t[MAX], double x[MAX], string fileName){
     // creating output stream
     ofstream fout(fileName);
     if (fout.fail()){ // reporting failed write
@@ -53,19 +85,4 @@ void saveFile(float t[MAX], float x[MAX], string fileName){
     }
 
     return;
-}
-
-int main(){
-    readFile("params.dat");
-    // calculating number of steps in 9 seconds
-    steps=int(9/dt);
-    // creating arrays for time and position
-    float t[MAX];
-    float x[MAX];
-    run(t,x);
-
-
-
-
-    return 0;
 }
