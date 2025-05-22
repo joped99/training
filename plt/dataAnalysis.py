6ae6d6b7fa9cec5107343ea346d46034329760a4
Joseph Whittier <josephwhittier.23@gmail.com>
2025-05-22 11:28:03 -0600

    modified:   plt/dataAnalysis.py 	modified:   varyStep.sh

--- plt/dataAnalysis.py
+++ plt/dataAnalysis.py
@@ -7,47 +7,56 @@ def exponential(x):
 
 # reading data
 out0 = [[float(x) for x in line.split()] # dt=10^0 run
-        for line in open("../data/output0.dat", "r").readlines()]
+        for line in open("data/output0.dat", "r").readlines()]
 
 out1 = [[float(x) for x in line.split()] # dt=10^-1 run
-        for line in open("../data/output1.dat", "r").readlines()]
+        for line in open("data/output1.dat", "r").readlines()]
 
 out2 = [[float(x) for x in line.split()] # dt=10^-2 run
-        for line in open("../data/output2.dat", "r").readlines()]
+        for line in open("data/output2.dat", "r").readlines()]
 
 out3 = [[float(x) for x in line.split()] # dt=10^-3 run
-        for line in open("../data/output3.dat", "r").readlines()]
+        for line in open("data/output3.dat", "r").readlines()]
 
 out4 = [[float(x) for x in line.split()] # dt=10^-4 run
-        for line in open("../data/output4.dat", "r").readlines()]
+        for line in open("data/output4.dat", "r").readlines()]
 
 out5 = [[float(x) for x in line.split()] # dt=10^-5 run
-        for line in open("../data/output5.dat", "r").readlines()]
+        for line in open("data/output5.dat", "r").readlines()]
 
-# out6 = [[float(x) for x in line.split()] # dt=10^-6 run
-#         for line in open("../data/output6.dat", "r").readlines()]
+out6 = [[float(x) for x in line.split()] # dt=10^-6 run
+        for line in open("data/output6.dat", "r").readlines()]
 
 # transposing data for plotting
-out0=out0.T
-out1=out1.T
-out2=out2.T
-out3=out3.T
-out4=out4.T
-out5=out5.T
-# out6=out6.T
+data0=np.copy(out0)
+data1=np.copy(out1)
+data2=np.copy(out2)
+data3=np.copy(out3)
+data4=np.copy(out4)
+data5=np.copy(out5)
+data6=np.copy(out6)
+
+data0=np.transpose(data0)
+data1=np.transpose(data1)
+data2=np.transpose(data2)
+data3=np.transpose(data3)
+data4=np.transpose(data4)
+data5=np.transpose(data5)
+data6=np.transpose(data6)
 
 # creating data for analytic solution
-trueExp=exponential(out5[0])
+trueExp=exponential(data4[0])
 
 # plotting
-plt.plot(out0[0],out0[1],label="dt=1e0")
-plt.plot(out1[0],out1[1],label="dt=1e-1")
-plt.plot(out2[0],out2[1],label="dt=1e-2")
-plt.plot(out3[0],out3[1],label="dt=1e-3")
-plt.plot(out4[0],out4[1],label="dt=1e-4")
-plt.plot(out5[0],out5[1],label="dt=1e-5")
-# plt.plot(out6[0],out6[1],label="dt=1e-6")
-plt.plot(out5[0],trueExp,label="Analytic")
+# plt.plot(data0[0],data0[1],label="dt=1e0")
+plt.plot(data1[0],data1[1],label="dt=1e-1")
+plt.plot(data2[0],data2[1],label="dt=1e-2")
+plt.plot(data3[0],data3[1],label="dt=1e-3")
+plt.plot(data4[0],data4[1],label="dt=1e-4")
+# plt.plot(data5[0],data5[1],label="dt=1e-5")
+# plt.plot(data6[0],data6[1],label="dt=1e-6")
+plt.plot(data4[0],trueExp,label="Analytic")
 plt.legend()
 plt.title("Comparison of Step Sizes")
-plt.show()
+plt.savefig("plt/exp_comp.png")
+plt.close()
--- varyStep.sh
+++ varyStep.sh
@@ -7,7 +7,7 @@
 
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
-export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+# export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
